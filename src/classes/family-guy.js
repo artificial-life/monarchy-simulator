b5b64dcb641721cd93d0437f56223fdc98b72ce1
artificial-life <proteus.protocol@gmail.com>
2016-09-18 23:17:28 +0600

    fa.

--- src/classes/family-guy.js
+++ src/classes/family-guy.js
@@ -1,20 +1,44 @@
 'use strict'
 
+var VISIT_INTERVAL = 2000; //@NOTE: msec
+
 var inherits = require('util').inherits;
 
 var Puppet = require('./puppet.js');
+var Pedigree = require('./pedigree.js');
 
 //@NOTE: implements family relationships
 
-function FamilyGuy(name, client) {
-	FamilyGuy.super_.prototype.constructor.call(this, name, client);
+function FamilyGuy(name, family, client) {
+  FamilyGuy.super_.prototype.constructor.call(this, name, client);
+  this.pedigree = new Pedigree(family, client);
+
+  this.pedigree.add(name, function() {
+
+  });
+
+  var self = this;
+
+  this.visit = setInterval(function() {
+    self.visitRelative(self.relativeStatus);
+  }, VISIT_INTERVAL);
 }
 
 inherits(FamilyGuy, Puppet);
 
+FamilyGuy.prototype.relativeStatusHandler = function(callback) {
+  this.relativeStatus = callback;
+};
 
-FamilyGuy.prototype.visitRelative = function() {
+FamilyGuy.prototype.visitRelative = function(callback) {
+  this.pedigree.getRelative(function(err, res) {
+    if (err) throw new Error(err);
+    if (res == null) {
 
+      return;
+    }
+
+  })
 };
 
 FamilyGuy.prototype.hostRelative = function() {
@@ -26,7 +50,7 @@ FamilyGuy.prototype._addToPedigree = function() {
 };
 
 FamilyGuy.prototype._correctPedigree = function() {
-	//@NOTE: when relative dead
+  //@NOTE: when relative dead
 };
 
 module.exports = FamilyGuy;
--- src/classes/royalty.js
+++ src/classes/royalty.js
@@ -9,32 +9,32 @@ var ERROR_LIST = 'failed-orders';
 //@NOTE: figths for the crown
 
 function Royalty(name, client) {
-	Royalty.super_.prototype.constructor.call(this, name, client);
-	var self = this;
+  Royalty.super_.prototype.constructor.call(this, name, 'royal-family', client);
+  var self = this;
 
-	this.queue.act('royal-orders', function(msg) {
-		if (self.commandHandler instanceof Function) self.commandHandler(msg, self.errorHandler);
-	});
+  this.queue.act('royal-orders', function(msg) {
+    if (self.commandHandler instanceof Function) self.commandHandler(msg, self.errorHandler);
+  });
 }
 
 inherits(Royalty, FamilyGuy);
 
 Royalty.prototype.onCommand = function(callback) {
-	this.commandHandler = callback;
+  this.commandHandler = callback;
 };
 
 Royalty.prototype.errorHandler = function(error, msg) {
-	if (!error) return;
-	this.client.rpush(ERROR_LIST, msg);
+  if (!error) return;
+  this.client.rpush(ERROR_LIST, msg);
 };
 
 Royalty.prototype.drainErrors = function() {
-	var count = 0;
-	this.queue.drain(ERROR_LIST, function() {
-		count++
-	}, function(err, res) {
-		console.log('%d total errors drained', count);
-	})
+  var count = 0;
+  this.queue.drain(ERROR_LIST, function() {
+    count++
+  }, function(err, res) {
+    console.log('%d total errors drained', count);
+  })
 };
 
 Royalty.prototype.relativeDead = function() {
